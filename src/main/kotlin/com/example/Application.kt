aedc1ba05ce6f621bb38a5ee5d522283a81833fb
juanchovelezpro <velezolaya2012@hotmail.com>
2024-11-02 00:51:49 -0500

    Testing Frame.Text to send messages in real-time and communicating UI<->Agents

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,7 +1,5 @@
 package com.example
 
-import com.example.model.Command
-import com.example.model.Response
 import com.example.plugins.*
 import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.*
@@ -9,57 +7,69 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.routing.routing
 import io.ktor.server.websocket.WebSockets
-import io.ktor.server.websocket.pingPeriod
-import io.ktor.server.websocket.receiveDeserialized
-import io.ktor.server.websocket.sendSerialized
-import io.ktor.server.websocket.timeout
 import io.ktor.server.websocket.webSocket
 import io.ktor.websocket.DefaultWebSocketSession
+import io.ktor.websocket.Frame
+import io.ktor.websocket.readText
+import io.ktor.websocket.send
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         install(WebSockets) {
             contentConverter = KotlinxWebsocketSerializationConverter(Json)
-            pingPeriod = 15.toDuration(DurationUnit.SECONDS)
-            timeout = 15.toDuration(DurationUnit.SECONDS)
+            //pingPeriod = 15.toDuration(DurationUnit.SECONDS)
+            //timeout = 15.toDuration(DurationUnit.SECONDS)
             maxFrameSize = Long.MAX_VALUE
             masking = false
         }
         routing {
+            val uiSessions = ConcurrentHashMap<String, DefaultWebSocketSession>()
             val clientSessions = ConcurrentHashMap<String, DefaultWebSocketSession>()
-
+            var clientId = ""
+            var uiSessionId = ""
             webSocket("/connect") {
-                val clientId = call.request.queryParameters["clientId"] ?: generateClientId()
+                clientId = call.request.queryParameters["clientId"] ?: generateClientId()
                 clientSessions[clientId] = this
                 println("Client connected: $clientId")
 
                 try {
-                    launch {
-                        while (true) {
-                            delay(3000)
-                            val command = Command("ping", "Ping from Server")
-                            sendSerialized(command)
-                            println("Ping sent to client: $clientId")
-                        }
-                    }
+//                    launch {
+//                        while (true) {
+//                            delay(3000)
+//                            val command = Command("ping", "Ping from Server")
+//                            send("Ping")
+//                            println("Ping sent to client: $clientId")
+//                        }
+//                    }
 
-                    while (true) {
-                        incoming.consumeEach {
-                            val response = receiveDeserialized<Response>()
-                            println("Type:${response.type} Data:${response.data}")
+                    //while (true) {
+                    incoming.consumeEach {
+                        //val response = receiveDeserialized<Response>()
+                        //println("Type:${response.type} Data:${response.data}")
+                        if (it is Frame.Text) {
+                            uiSessions[uiSessionId]?.send(it.readText())
                         }
+
                     }
+                    //}
                 } catch (ex: Exception) {
                     ex.printStackTrace()
                 }
 
+            }
+
+            webSocket("/ui") {
+                uiSessionId = generateClientId()
+                uiSessions[uiSessionId] = this
+                incoming.consumeEach {
+                    if (it is Frame.Text) {
+                        send("Retrieving ${it.readText()}")
+                        clientSessions[clientId]?.send(it.readText())
+                    }
+                }
             }
         }
 
--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -1,11 +1,8 @@
 package com.example.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.routing.routing
 import io.ktor.server.websocket.*
-import io.ktor.websocket.DefaultWebSocketSession
 import kotlinx.coroutines.*
-import java.util.concurrent.ConcurrentHashMap
 
 fun Application.configureSockets(coroutineScope: CoroutineScope) {
     install(WebSockets) {
